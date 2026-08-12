08b65d05d3e95b6aeb78d2ddb7274ebf83dc2144
sakshamtiwari2006 <saksham4141tiwari@gmail.com>
2026-08-13 00:56:31 +0530

    Update write_side.v

--- write_side.v
+++ write_side.v
@@ -1,43 +1,41 @@
 module write_side(
-    input wclk,
-    input wrst_n,
-    input w_en,
-    input [3:0] g_rptr_sync,
+    input        wclk,
+    input        wrst_n,
+    input        w_en,
+    input [7:0]  data_in,
+    input [3:0]  g_rptr_sync,
 
     output reg [3:0] g_wptr,
     output reg [3:0] b_wptr,
-    output full
+    output reg       full
 );
 
     reg [3:0] b_wptr_next;
     reg [3:0] g_wptr_next;
+    wire full_val;
 
-    
+    always @(*) begin
+        b_wptr_next = b_wptr;
 
-    always @(w_en) 
-        begin
-            b_wptr_next = b_wptr;
+        if (w_en && (data_in != 8'h00) && !full)
+            b_wptr_next = b_wptr + 1'b1;
 
-            if (w_en && !full) begin
-                b_wptr_next = b_wptr + 1;
-            end
+        g_wptr_next = (b_wptr_next >> 1) ^ b_wptr_next;
+    end
 
-            g_wptr_next = (b_wptr_next >> 1) ^ b_wptr_next;
+    assign full_val = (g_wptr_next == {~g_rptr_sync[3:2], g_rptr_sync[1:0]});
+
+    always @(posedge wclk or negedge wrst_n) begin
+        if (!wrst_n) begin
+            b_wptr <= 4'h0;
+            g_wptr <= 4'h0;
+            full   <= 1'b0;
         end
-
-    always @(posedge wclk)
-        begin
-            if(!wrst_n) begin
-                b_wptr <= 0;
-                g_wptr <= 0;
-            end
-
-            else if(!full) begin
-                b_wptr <= b_wptr_next;
-                g_wptr <= g_wptr_next;
-            end
+        else begin
+            b_wptr <= b_wptr_next;
+            g_wptr <= g_wptr_next;
+            full   <= full_val;
         end
-
-    assign full =(g_wptr_next =={~g_rptr_sync[3],g_rptr_sync[2:0]});
+    end
 
 endmodule
